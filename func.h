adda97a4ec6f9d44cf779b84901db41a935c39bb
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-04-23 13:43:21 +0200

    Rimosso if-statement inutile, codice ottimizzato
    Ci si riporta sempre al caso a<b, variabile fx cambiata in fm per amor di precisione

--- func.h
+++ func.h
@@ -1,3 +1,4 @@
+#include <utility>
 
 double difazio(double, double, int);
 double func(double);
@@ -5,28 +6,16 @@ double func(double);
 
 double difazio(double a, double b, int c)
 {
+    if(b<a) std::swap(b, a);
     double m=(a+b)/2;
-    double fx=func(m);
-    if(a>b)
+    double fm=func(m); 
+    if(c>=0)
     {
-        if(c>=0)
-        {
-            if(fx==0)     {std::cout<<fx<<std::endl; return fx;}                 //se fa 0 siamo fortunati, non c'è bisogno di procedere
-            else if(fx>0) {std::cout<<fx<<std::endl; return difazio(a,m,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
-            else          {std::cout<<fx<<std::endl; return difazio(m,b,--c);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
-        }
-        else return fx;
+        if(fm==0)     {std::cout<<fm<<std::endl; return fm;}                 //se fa 0 siamo fortunati, non c'è bisogno di procedere
+        else if(fm>0) {std::cout<<fm<<std::endl; return difazio(a,m,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
+        else          {std::cout<<fm<<std::endl; return difazio(m,b,--c);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
     }
-    else
-    {
-        if(c>=0)
-        {
-            if(fx==0)     {std::cout<<fx<<std::endl; return fx;}                 //se fa 0 siamo fortunati, non c'è bisogno di procedere
-            else if(fx>0) {std::cout<<fx<<std::endl; return difazio(m,b,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
-            else          {std::cout<<fx<<std::endl; return difazio(a,m,--c);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
-        }
-        else return fx;
-    }
+    else return fm;
 }
 
 double func(double x)
