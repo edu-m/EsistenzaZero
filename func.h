a14dae09320043fec73520fbec554ebd69803f92
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-04-23 16:21:38 +0200

    Alternativo
    La funzione si ferma da sola appena il valore raggiunge un punto così basso da essere trascurabile ed approssimabile a 0.

--- func.h
+++ func.h
@@ -1,24 +1,25 @@
 #include <utility>
+#define LEGGE (x*x)-596
 
-double difazio(double, double, int);
+double difazio(double, double, double);
 double func(double);
 
 
-double difazio(double a, double b, int c)
+double difazio(double a, double b, double temp)
 {
     if(b<a) std::swap(b, a);
     double m=(a+b)/2;
     double fm=func(m); 
-    if(c>=0)
+    if(fm!=temp)
     {
-        if(fm==0)     {std::cout<<fm<<std::endl; return fm;}                 //se fa 0 siamo fortunati, non c'è bisogno di procedere
-        else if(fm>0) {std::cout<<fm<<std::endl; return difazio(a,m,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
-        else          {std::cout<<fm<<std::endl; return difazio(m,b,--c);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
+        if(fm==0)     {std::cout<<fm<<std::endl; return fm;}                    //se fa 0 siamo fortunati, non c'è bisogno di procedere
+        else if(fm>0) {std::cout<<fm<<std::endl; return difazio(a,m,fm);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
+        else          {std::cout<<fm<<std::endl; return difazio(m,b,fm);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
     }
     else return fm;
 }
 
 double func(double x)
 {
-    return (x*x)-3; //inserire qui la legge della funzione
+    return LEGGE; //inserire qui la legge della funzione
 }
