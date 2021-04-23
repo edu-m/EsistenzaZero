7ed9f08a3f2be205341ebb4584e5ca94ca8b8a6f
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-04-23 16:22:20 +0200

    main conciliato con func.h alternativo

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include "func.h"
-#define CONDITION func(a)*func(b)>0 || a==b || cont<=0
+#define CONDITION func(a)*func(b)>0 || a==b
 
 main()
 {
     double a,b;
-    int cont;
     do{
         using namespace std;
         cout<<"Inserisci il valore 'a' dell'intervallo> ";
         cin>>a;
         cout<<"Inserisci il valore 'b' dell'intervallo> ";
         cin>>b;
-        cout<<"Inserisci il numero di iterazioni> ";
-        cin>>cont;
         cout<<"f(a)= "<<func(a)<<endl;
         cout<<"f(b)= "<<func(b)<<endl;
         if(CONDITION) cout<<"Input non valido"<<endl;
@@ -21,6 +18,6 @@ main()
 
     std::cout<<"Premi qualunque tasto per procedere"<<std::endl;
     system("PAUSE>NUL");
-    difazio(a,b,cont);
+    difazio(a,b,0);
     return 0;
 }
