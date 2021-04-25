41268bd1cc0d7874d0af332bf513b86c6ae66093
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-04-25 12:38:01 +0200

    Algoritmo corretto

--- func.h
+++ func.h
@@ -12,7 +12,7 @@ double difazio(double a, double b, int c)
     if(c>=0)
     {
         if(fm==0)     {std::cout<<fm<<std::endl; return fm;}                 //se fa 0 siamo fortunati, non c'è bisogno di procedere
-        else if(fm>0) {std::cout<<fm<<std::endl; return difazio(a,m,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
+        else if(fm<0) {std::cout<<fm<<std::endl; return difazio(a,m,--c);}  //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
         else          {std::cout<<fm<<std::endl; return difazio(m,b,--c);} //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
     }
     else return fm;
