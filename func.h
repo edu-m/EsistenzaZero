6b83385cb4e4c742995988a92477684ccc77d533
Eduardo Meli <MLEDRD02E15C351O@studium.unict.it>
2021-04-27 14:40:28 +0200

    finetune del cap da 50 a 25

--- func.h
+++ func.h
@@ -7,19 +7,19 @@ double func(double);
 
 double difazio(double a, double b, double target)
 {
-    static int cap=50;
+    static int cap=25;
     double m=(a+b)/2;
     double fm=func(m); 
     if(cap>0)
     {
-        if(fm==target)     {std::cout<<CURRENT_ITERATION; return fm;}                         //se è uguale al target siamo fortunati, non c'è bisogno di procedere
+        if(fm==target)     {std::cout<<CURRENT_ITERATION; return fm;}                                //se è uguale al target siamo fortunati, non c'è bisogno di procedere
         else if(fm>target) {cap--; std::cout<<CURRENT_ITERATION; return difazio(a,m,target);}       //consideriamo la funzione tra "a" e il punto medio tra "a" e "b"
         else               {cap--; std::cout<<CURRENT_ITERATION; return difazio(m,b,target);}      //consideriamo la funzione tra il punto medio tra "a" e "b" e "b"
     }
     else
     {
         std::cout<<CURRENT_ITERATION;
-        //std::cout<<"Iterazione interrotta, valore approssimato"<<std::endl;  
+        std::cout<<"Iterazione interrotta, valore approssimato"<<std::endl;  
         return fm;
     }
 }
