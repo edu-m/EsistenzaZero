d549f57b67aca74a452e3f035de39780ba2f02cd
Eduardo Meli <MLEDRD02E15C351O@studium.unict.it>
2021-05-03 16:15:15 +0200

    code cleanup

--- main.cpp
+++ main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
 #include "func.h"
-#define CONDITION target<func(a) || target>func(b) || a==b
+#define CONDITION target<func(a) || target> func(b) || a == b
 
 main()
 {
-    double a,b,target;
-    do{
+    double a, b, target;
+    do
+    {
         using namespace std;
-        cout<<"Inserisci il valore 'a' dell'intervallo> ";
-        cin>>a;
-        cout<<"f(a)= "<<func(a)<<endl;
-        cout<<"Inserisci il valore 'b' dell'intervallo> ";
-        cin>>b;
-        cout<<"f(b)= "<<func(b)<<endl;
-        if(a>b) std::swap(b, a);
-        cout<<"Inserisci il valore obiettivo> ";
-        cin>>target; 
-        if(CONDITION) cout<<"Input non valido"<<endl;
-    }while(CONDITION);
+        cout << "Inserisci il valore 'a' dell'intervallo> ";
+        cin >> a;
+        cout << "f(a)= " << func(a) << endl;
+        cout << "Inserisci il valore 'b' dell'intervallo> ";
+        cin >> b;
+        cout << "f(b)= " << func(b) << endl;
+        if (a > b)
+            std::swap(b, a);
+        cout << "Inserisci il valore obiettivo> ";
+        cin >> target;
+        if (CONDITION)
+            cout << "Input non valido" << endl;
+    } while (CONDITION);
 
-    std::cout<<"Premi qualunque tasto per procedere"<<std::endl;
+    std::cout << "Premi qualunque tasto per procedere" << std::endl;
     system("PAUSE>NUL");
-    difazio(a,b,target);
+    difazio(a, b, target);
     system("PAUSE>NUL");
     return 0;
 }
